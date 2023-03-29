1a5574c1067b265def3c537e9a275104f5e18f64
wenqing <wenqing@kerrylan.com>
2023-03-29 14:57:18 +0800

    chore: remove useless plugins

--- packages/nuxt-module/plugins/svg-icon.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import SvgIcon from 'vue-symbol-icon' // svg component
-
-Vue.use(SvgIcon) // register globally
-
-const req = require.context('~/assets/icons/svg', false, /\.svg$/)
-const requireAll = (requireContext) => requireContext.keys().map(requireContext)
-requireAll(req)
